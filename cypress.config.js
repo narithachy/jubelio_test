13f6d6d83bdb8a1659eccd54940a23ac0ad7b9da
Naritha Cahya <ids.narithac@xl.co.id>
2025-02-09 13:04:09 +0700

    update terbaru

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,9 @@
 const { defineConfig } = require('cypress')
 module.exports = defineConfig({
     e2e: {
-        specPattern : "cypress/support/e2e",
+        specPattern : "cypress/support/e2e/**/*.{spec,cy}.js",
         supportFile : false,
+        import      :'cypress-xpath'
     }
     
 })
